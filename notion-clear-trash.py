a7ff2d33d7df70aa0e3a05ae57d87558f6889a25
Guido Knoop <29152658+guidoknoop@users.noreply.github.com>
2021-01-21 13:09:05 +0100

    Updated to new api URL
    This one will work again.

--- notion-clear-trash.py
+++ notion-clear-trash.py
@@ -1,22 +1,30 @@
 from notion.client import NotionClient
 
-
 def get_trash(client):
-    query = {
-        'query': '',
-        'limit': 1000,
-        'spaceId': client.current_space.id
-    }
-    results = client.post('/api/v3/searchTrashPages', query)
+    query = {'type':'BlocksInSpace',
+             'query':'',
+             'filters':
+                {'isDeletedOnly':True,
+                 'excludeTemplates':False,
+                 'isNavigableOnly':True,
+                 'requireEditPermissions':False,
+                 'ancestors':[],
+                 'createdBy':[],
+                 'editedBy':[],
+                 'lastEditedTime':{},
+                 'createdTime':{}},
+             'sort':'Relevance',
+             'limit':1000,
+             'spaceId':client.current_space.id,
+             'source':'trash'}
+    results = client.post('/api/v3/search', query)
     block_ids = results.json()['results']
 
     return block_ids
 
-
 def delete_permanently(client, block_ids):
     for block_id in block_ids:
-        client.post("deleteBlocks", {"blockIds": [block_id], "permanentlyDelete": True})
-
+        client.post('/api/v3/deleteBlocks', {'blockIds': [block_id['id']], 'permanentlyDelete': True})
 
 if __name__== "__main__":
     token = input('Please enter your auth token: ')
